6b3b7bc6c7a53ef124671ceae94f0d83219275a7
Brian Duong <brianminhduong@gmail.com>
2022-01-22 05:30:21 +0000

    Add Paramerterized Queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,4 +1,14 @@
 const { Pool } = require("pg");
+const cohortName = process.argv[2];
+const queryString = `SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
+FROM assistance_requests
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+JOIN teachers ON teachers.id = teacher_id
+WHERE cohorts.name LIKE $1
+ORDER BY teacher;`;
+
+const values = [`%${cohortName}%`];
 
 //Database settings
 const pool = new Pool({
@@ -13,15 +23,7 @@ pool.connect();
 
 //Query search
 pool
-  .query(
-    `SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
-    FROM assistance_requests
-    JOIN students ON students.id = student_id
-    JOIN cohorts ON cohorts.id = cohort_id
-    JOIN teachers ON teachers.id = teacher_id
-    WHERE cohorts.name = '${process.argv[2]}'
-    ORDER BY teacher;`
-  )
+  .query(queryString, values)
   .then((res) => {
     res.rows.forEach((user) => {
       console.log(`${user.cohort}: ${user.teacher}`);
